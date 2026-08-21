b421250e61fcfd52aae8a6809f5cbf74c83d96b4
Newton Okeyo <okeyonewton18@gmail.com>
2026-08-21 18:52:11 +0300

    Fix typo in Supabase project URL domain

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rldmoznevnfigfxarqea.supabase.co';
+const supabaseUrl = 'https://rldmoznevfnigfxarqea.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsZG1vem5ldmZuaWdmeGFycWVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcxNTAwODMsImV4cCI6MjEwMjcyNjA4M30.yh1RZos2i3cpntWQttRPS_GhVwCzaT-9u_ssaCQ6Kew';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
